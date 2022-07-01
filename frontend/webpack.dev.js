eb153e1522c6cc6f1a438448e53cf40d61cbe104
Andrew Moss <andrew.moss@red-gate.com>
2022-07-01 14:22:40 +0100

    fix hot reloading

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,20 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = {
   entry: './src/index.tsx',
+  mode: 'development',
+  devServer: {
+    hot: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
-      },
       {
         enforce: "pre",
         test: /\.js$/,
@@ -24,11 +26,20 @@ module.exports = {
         test: /\.css$/,
         loader: "css-loader",
       },
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          plugins: ["react-refresh/babel"],
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 }
